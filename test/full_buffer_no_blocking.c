ad83a1f5e16ca7cf665defc0f3a494f2673c0cd5
Jeelm22 <112936181+Jeelm22@users.noreply.github.com>
2024-03-22 11:08:49 +0100

    Update commends full_buffer_no_blocking.c

--- test/full_buffer_no_blocking.c
+++ test/full_buffer_no_blocking.c
@@ -8,26 +8,32 @@
 #include "dm510_ioctl_commands.h"
 
 int main(int argc, char const *argv[]) {
+    //Open the device file for read-write access, in non-blocking mode
     int write_pointer = open("/dev/dm510-0", O_RDWR | O_NONBLOCK);
+    //Check if the device file failed to open
     if (write_pointer < 0) {
         perror("Failed to open device");
+        //Retrun 1 to indicate a failure
         return 1;
     }
-
+    //Retrieve the current size of the device buffer with ioctl command
     const int size = ioctl(write_pointer, GET_BUFFER_SIZE);
-    char n = 0;
-    size_t i = 0;
+    char n = 0;    //Initialize character to write to the device
+    size_t i = 0;    //Counter for the loop
 
+    //Loop to fill the device buffer
     while (i < size) {
+        //Write character to device
         write(write_pointer, &n, sizeof(n));
-        n++;
-        i++;
+        n++;    //Increment the character 
+        i++;    //Increment the loop counter
     }
-
+    //Check if the buffer is full, by writing once more
     if (write(write_pointer, &n, sizeof(n)) < 0) {
+        //Print error messaeges if it failes
         printf("Buffer is full\n");
     }
-
-    close(write_pointer); // Close the device file descriptor
+    // Close the device file descriptor
+    close(write_pointer); 
     return 0;
 }
