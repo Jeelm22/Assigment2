6c6d8c9c4554c1d813148dcd5e1adebbf9ad0bb1
Jeelm22 <112936181+Jeelm22@users.noreply.github.com>
2024-03-22 10:44:19 +0100

    add comments

--- test/too_many_writers.c
+++ test/too_many_writers.c
@@ -7,16 +7,23 @@
 #include "dm510_ioctl_commands.h"
 
 int main(int argc, char const *argv[]) {
-    size_t i = 0; // Initialize loop counter outside the loop
+    //Initialize loop counter, for looping through devices
+    size_t i = 0; 
+    //While loop to attempt opening DEVICE_COUNT number of device files
     while (i < DEVICE_COUNT) {
+        //Try to open with read and write premission
         int write_pointer = open("/dev/dm510-0", O_RDWR);
         printf("Write pointer %lu : %d ", i, write_pointer);
+        //Check if the file descriptor is invalid
         if (0 > write_pointer) {
             printf("invalid pointer!\n");
+            //Exit the program with an error state if falied
             return 0;
         }
+        //Print newline if the file descriptor is valid
         printf("\n");
         i++; // Increment loop counter
     }
+    //Exit the program succesfully, if there loop completes without errors
     return 0;
 }
