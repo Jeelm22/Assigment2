d1dda6b2116d9dfd5956369e47af83d5aae0377c
Jeelm22 <112936181+Jeelm22@users.noreply.github.com>
2024-04-04 14:20:23 +0200

    Update read_empty_buffer_no_block.c

--- test/read_empty_buffer_no_block.c
+++ test/read_empty_buffer_no_block.c
@@ -4,39 +4,41 @@
 #include <string.h>
 #include <errno.h>
 
-#define DEVICE_FILE "/dev/dm510-0"
 //Defined number of bytes to read
 #define READ_SIZE 42
 
 int main() {
     //Open the device with read-write acces, in non-blocking mode
-    int fd = open(DEVICE_FILE, O_RDWR | O_NONBLOCK);
+    int read = open("/dev/dm510-0", O_RDWR | O_NONBLOCK);
     //Allocate buffer for the data read from device, +1 for null termination
     char buffer[READ_SIZE + 1]; 
 
     //Check if the device file was successfully opened
-    if (fd < 0) {
+    if (read < 0) {
         fprintf(stderr, "Failed to open device: %s\n", strerror(errno));
         //Returnn 1 to indicate an error condition
         return 1;
     }
     //Read from the device file
-    ssize_t bytes_read = read(fd, buffer, READ_SIZE);
+    ssize_t bytes_read = read(read, buffer, READ_SIZE);
     //Check if the read operation was successful
     if (bytes_read < 0) {
         //First cheeck if it failed due to the operation being non-blocking
         if (errno == EAGAIN || errno == EWOULDBLOCK) {
-            printf("Non-blocking read returned immediately with no data.\n");     
+            printf("Buffer is empty, no data to read\n");     
         } else {
             //If it's for another reason print error message
             fprintf(stderr, "Read error: %s\n", strerror(errno));
+            close(read);
+            return 2; //return 2 to indicate a different error
         }
     } else {
-        //If the read was a succes, null-terminate the string and print it
+        //If the read was a succes(which should not happen), null-terminate the string
         buffer[bytes_read] = '\0'; // Ensure the string is null-terminated
-        printf("Received: '%s'\n", buffer);
+        close(read);
+        return 3 // Return 3 to indicate unexpected success
     }
 
-    close(fd);
+    close(read);
     return 0;
 }
