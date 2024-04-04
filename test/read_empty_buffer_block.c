7f982f37c61d51fa8fa9bc2056374d459993078a
Jeelm22 <112936181+Jeelm22@users.noreply.github.com>
2024-04-04 14:26:37 +0200

    Update read_empty_buffer_block.c

--- test/read_empty_buffer_block.c
+++ test/read_empty_buffer_block.c
@@ -4,24 +4,23 @@
 #include <string.h>
 #include <errno.h>
 
-#define DEVICE_FILE "/dev/dm510-0"
 //Define number of bytes to read
 #define READ_SIZE 42
 
 int main() {
     //Open the device file for both reading and writing
-    int fd = open(DEVICE_FILE, O_RDWR);
+    int read_pointer = open("/dev/dm510-0", O_RDWR);
     //Allocate buffer to store the data read, +1 byte for null termination
     char buffer[READ_SIZE + 1]; // +1 for null termination
 
     //Check if opening the device file failed
-    if (fd < 0) {
+    if (read_pointer < 0) {
         fprintf(stderr, "Failed to open device: %s\n", strerror(errno));
         //Return 1 for failure
         return 1;
     }
     //Read up to READ_SIZE bytes from the device file into the buffer
-    ssize_t bytes_read = read(fd, buffer, READ_SIZE);
+    ssize_t bytes_read = read(read_pointer, buffer, READ_SIZE);
     //Check if read operation failed
     if (bytes_read < 0) {
         //If it failed, print error message
@@ -33,7 +32,6 @@ int main() {
         printf("Received: '%s'\n", buffer);
     }
 
-    close(fd);
+    close(read_pointer);
     return 0;
 }
-
