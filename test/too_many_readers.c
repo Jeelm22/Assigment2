014640be50b9f87d8618ec9536921d8462ef5a37
Jeelm22 <112936181+Jeelm22@users.noreply.github.com>
2024-04-04 03:02:14 +0200

    Update too_many_readers.c

--- test/too_many_readers.c
+++ test/too_many_readers.c
@@ -8,12 +8,10 @@
 #include <sys/ioctl.h>
 
 int main(int argc, char const *argv[]) {
-printf("1:Size of int in user space: %zu bytes\n", sizeof(int));
     int desired_max_readers = 9; // Change this to the expected max readers
     int max_readers; // This will hold the maximum number of readers allowed
     int ret; // To capture return values of system calls
     int *fds; // Array to keep track of file descriptors
-printf("2:Size of int in user space: %zu bytes\n", sizeof(int));
 
     // Open the device file in read-only mode
     int read_pointer = open("/dev/dm510-0", O_RDONLY);
@@ -25,28 +23,22 @@ printf("2:Size of int in user space: %zu bytes\n", sizeof(int));
     // Set the maximum number of reader processes
     printf("Setting max amount of readers to: %d\n", desired_max_readers);
     ret = ioctl(read_pointer, SET_MAX_NR_PROCESSES, &desired_max_readers);
-    printf("Setting max amount of readers to: %d\n", desired_max_readers);
-    printf("ioctl SET_MAX_NR_PROCESSES return value: %d\n", ret);
     if (ret < 0) {
         perror("Failed to set the maximum number of reader processes");
         close(read_pointer);
         return EXIT_FAILURE;
     }
-printf("Setting max amount of readers to: %d\n", desired_max_readers);
 
     // Get the new max readers value to confirm it was set correctly
     ret = ioctl(read_pointer, GET_MAX_NR_PROCESSES, &max_readers);
-    printf("ioctl GET_MAX_NR_PROCESSES return value: %d, max readers: %d\n", ret, max_readers);
     if (ret < 0) {
         perror("Failed to get the maximum number of reader processes");
         close(read_pointer);
         return EXIT_FAILURE;
     }
-    printf("Max amount of readers set to: %d\n", max_readers);
 
     // Allocate memory to keep track of file descriptors
     fds = malloc(sizeof(int) * (max_readers + 1));
-    printf("Setting max amount of readers to: %d\n", max_readers);
     if (!fds) {
         perror("Failed to allocate memory for file descriptors");
         close(read_pointer);
@@ -60,7 +52,6 @@ printf("Setting max amount of readers to: %d\n", desired_max_readers);
 
     // Open device files up to the maximum number of readers
     for (int i = 1; i <= max_readers + 1; i++) {
-        printf("Setting max amount of readers to: %d\n", max_readers);
         fds[i] = open("/dev/dm510-0", O_RDONLY);
         printf("Read pointer %d : %d ", i, fds[i]);
         if (fds[i] < 0) {
